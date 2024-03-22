e2a889c8408e1b733a87f903ff582c4c2e93d4c0
Alan Reis <alanreisanjo@gmail.com>
2024-03-22 12:45:27 -0300

    Adição da metadata dinâmica

--- app/fruit/[id]/page.tsx
+++ app/fruit/[id]/page.tsx
@@ -1,6 +1,19 @@
 import Fruits from "@/services/Fruits";
 import { PageParams } from "./types";
 import Link from "next/link";
+import { Metadata } from "next";
+
+export async function generateMetadata(
+  { params }: PageParams,
+): Promise<Metadata> {
+  const { id } = params;
+
+  const data = await Fruits.getById(Number(id));
+
+  return {
+    title: (data.success) ? `${ data?.data } | Project` : "Fruit Not Found | Project",
+  };
+}
 
 export default async function Page({ params }: PageParams) {
   const { id } = params;
