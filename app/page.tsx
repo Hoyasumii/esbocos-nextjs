78557c972ebfbfad810fd63ab9d2a6d731733a40
Alan Reis <alanreisanjo@gmail.com>
2024-10-31 08:48:54 -0300

    Sei lá

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import slugify from "slugify";
 import { List } from "antd";
 import Item from "antd/es/list/Item";
 import Link from "next/link";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home | Testando Next",
+};
 
 export default async function Page() {
   const data = await Fruits.getAll();
